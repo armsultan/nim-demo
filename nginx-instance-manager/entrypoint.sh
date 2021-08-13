ab84058cdbce22efc5f93a9ddbb17f6bc1d75d65
Artie Jurgenson <artjur@gmail.com>
2021-08-13 15:48:46 -0400

    added env vars to NIM service and modified the entrypoint script to poll until the NIM endpoint is up to attach the agent

--- nginx-instance-manager/entrypoint.sh
+++ nginx-instance-manager/entrypoint.sh
@@ -6,8 +6,19 @@ set -m
 /usr/sbin/nginx -g 'daemon off;' &
 # start nim
 /usr/sbin/nginx-manager &
+declare -r HOST="${COMPASS_PROTOCOL}://${COMPASS_SERVER}:${COMPASS_PORT}"
+wait-for-url() {
+    echo "Testing $1"
+    timeout -s TERM ${COMPASS_TIMEOUT} bash -c \
+    'while [[ "$(curl -s -k -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
+    do echo "Waiting for ${0}" && sleep 2;\
+    done' ${1}
+    echo "OK!"
+    curl -I -k "$1"
+}
+wait-for-url "${HOST}"
+echo "$HOSTNAME"
 # Start the helper process
-sleep 45
 /usr/sbin/nginx-agent
 # now we bring the primary process back into the foreground
 # and leave it there
